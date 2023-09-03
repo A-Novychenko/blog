eef9d80513c575b038910fecb6016ad2965c7f44
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-09-03 18:22:16 +0300

    rev

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -23,7 +23,7 @@ export default async function FeedPage() {
   const res = await fetch(`${NEXTAUTH_URL}/api/post/list`, {
     // const res = await fetch("http://localhost:3000/api/post/list", {
     cache: "reload",
-    // next: {revalidate: 1},
+    next: {revalidate: 1},
   });
 
   const {posts} = await res.json();
